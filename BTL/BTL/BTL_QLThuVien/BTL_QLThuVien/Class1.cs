afed95f4f90feda9ee17104b96a37ec8d3b11a5b
LeDucAnh2012 <ducanh1001110@gmail.com>
2021-05-04 17:04:07 +0700

    update file ADD Class1.cs

---
+++ BTL/BTL/BTL_QLThuVien/BTL_QLThuVien/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_QLThuVien
+{
+    class Class1
+    {
+        string name;
+
+        public string Name { get => name; set => name = value; }
+    }
+    
+}
