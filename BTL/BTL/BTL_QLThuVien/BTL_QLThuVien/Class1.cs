b3cd2320f3585c5c22a2b5df482b9496a31022d4
LeDucAnh2012 <ducanh1001110@gmail.com>
2021-05-06 16:33:25 +0700

    change Class1 file

--- BTL/BTL/BTL_QLThuVien/BTL_QLThuVien/Class1.cs
+++ BTL/BTL/BTL_QLThuVien/BTL_QLThuVien/Class1.cs
@@ -8,12 +8,7 @@ namespace BTL_QLThuVien
 {
     class Class1
     {
-        string name;
-        string age;
-        string dateOfBirth;
-        public string Name { get => name; set => name = value; }
-        public string Age { get => age; set => age = value; }
-        public string DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
+       
     }
     
 }
