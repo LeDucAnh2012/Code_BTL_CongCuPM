05f360f08268c2f9aef8a6d197aea7f6a1bf9c63
LeDucAnh2012 <ducanh1001110@gmail.com>
2021-05-07 00:25:54 +0700

    main code

---
+++ BTL/BTL_QLThuVien/BTL_QLThuVien/fResistration.Designer.cs
@@ -0,0 +1,332 @@
+﻿
+namespace BTL_QLThuVien
+{
+    partial class fResistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelResistration = new System.Windows.Forms.Panel();
+            this.dateTimePickerNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerNgayDangKy = new System.Windows.Forms.DateTimePicker();
+            this.txtTenDocGia = new System.Windows.Forms.TextBox();
+            this.txtMaDocGia = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnCancelBorrow = new System.Windows.Forms.Button();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.txtCCCD = new System.Windows.Forms.TextBox();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelResistration.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelResistration
+            // 
+            this.panelResistration.Controls.Add(this.dateTimePickerNgaySinh);
+            this.panelResistration.Controls.Add(this.dateTimePickerNgayDangKy);
+            this.panelResistration.Controls.Add(this.txtTenDocGia);
+            this.panelResistration.Controls.Add(this.txtMaDocGia);
+            this.panelResistration.Controls.Add(this.txtMatKhau);
+            this.panelResistration.Controls.Add(this.txtTenDangNhap);
+            this.panelResistration.Controls.Add(this.label1);
+            this.panelResistration.Controls.Add(this.label3);
+            this.panelResistration.Controls.Add(this.label4);
+            this.panelResistration.Controls.Add(this.label5);
+            this.panelResistration.Controls.Add(this.label11);
+            this.panelResistration.Controls.Add(this.btnCancelBorrow);
+            this.panelResistration.Controls.Add(this.btnDangKy);
+            this.panelResistration.Controls.Add(this.txtCCCD);
+            this.panelResistration.Controls.Add(this.txtSDT);
+            this.panelResistration.Controls.Add(this.txtDiaChi);
+            this.panelResistration.Controls.Add(this.label9);
+            this.panelResistration.Controls.Add(this.label8);
+            this.panelResistration.Controls.Add(this.label7);
+            this.panelResistration.Controls.Add(this.label6);
+            this.panelResistration.Controls.Add(this.label2);
+            this.panelResistration.Location = new System.Drawing.Point(12, 12);
+            this.panelResistration.Name = "panelResistration";
+            this.panelResistration.Size = new System.Drawing.Size(776, 393);
+            this.panelResistration.TabIndex = 0;
+            // 
+            // dateTimePickerNgaySinh
+            // 
+            this.dateTimePickerNgaySinh.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerNgaySinh.Location = new System.Drawing.Point(148, 218);
+            this.dateTimePickerNgaySinh.Name = "dateTimePickerNgaySinh";
+            this.dateTimePickerNgaySinh.Size = new System.Drawing.Size(222, 20);
+            this.dateTimePickerNgaySinh.TabIndex = 6;
+            // 
+            // dateTimePickerNgayDangKy
+            // 
+            this.dateTimePickerNgayDangKy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePickerNgayDangKy.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerNgayDangKy.Location = new System.Drawing.Point(547, 92);
+            this.dateTimePickerNgayDangKy.Name = "dateTimePickerNgayDangKy";
+            this.dateTimePickerNgayDangKy.Size = new System.Drawing.Size(222, 20);
+            this.dateTimePickerNgayDangKy.TabIndex = 9;
+            // 
+            // txtTenDocGia
+            // 
+            this.txtTenDocGia.Location = new System.Drawing.Point(148, 177);
+            this.txtTenDocGia.Multiline = true;
+            this.txtTenDocGia.Name = "txtTenDocGia";
+            this.txtTenDocGia.Size = new System.Drawing.Size(220, 25);
+            this.txtTenDocGia.TabIndex = 5;
+            // 
+            // txtMaDocGia
+            // 
+            this.txtMaDocGia.Location = new System.Drawing.Point(148, 135);
+            this.txtMaDocGia.Multiline = true;
+            this.txtMaDocGia.Name = "txtMaDocGia";
+            this.txtMaDocGia.Size = new System.Drawing.Size(220, 25);
+            this.txtMaDocGia.TabIndex = 4;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(150, 92);
+            this.txtMatKhau.Multiline = true;
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(220, 25);
+            this.txtMatKhau.TabIndex = 3;
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Location = new System.Drawing.Point(148, 50);
+            this.txtTenDangNhap.Multiline = true;
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(220, 25);
+            this.txtTenDangNhap.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 48;
+            this.label1.Text = "Ngày Sinh";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 20);
+            this.label3.TabIndex = 47;
+            this.label3.Text = "Tên Độc Giả";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label4.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 20);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Mã Độc Giả";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label5.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(7, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 20);
+            this.label5.TabIndex = 45;
+            this.label5.Text = "Mật Khẩu";
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label11.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(7, 50);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(128, 20);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "Tên Đăng Nhập";
+            // 
+            // btnCancelBorrow
+            // 
+            this.btnCancelBorrow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancelBorrow.BackColor = System.Drawing.Color.Aqua;
+            this.btnCancelBorrow.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelBorrow.Location = new System.Drawing.Point(413, 350);
+            this.btnCancelBorrow.Name = "btnCancelBorrow";
+            this.btnCancelBorrow.Size = new System.Drawing.Size(112, 32);
+            this.btnCancelBorrow.TabIndex = 12;
+            this.btnCancelBorrow.Text = "Cancel";
+            this.btnCancelBorrow.UseVisualStyleBackColor = false;
+            this.btnCancelBorrow.Click += new System.EventHandler(this.btnCancelBorrow_Click);
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDangKy.BackColor = System.Drawing.Color.Aqua;
+            this.btnDangKy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangKy.Location = new System.Drawing.Point(191, 350);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(112, 32);
+            this.btnDangKy.TabIndex = 11;
+            this.btnDangKy.Text = "Đăng Ký";
+            this.btnDangKy.UseVisualStyleBackColor = false;
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
+            // 
+            // txtCCCD
+            // 
+            this.txtCCCD.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCCCD.Location = new System.Drawing.Point(549, 134);
+            this.txtCCCD.Multiline = true;
+            this.txtCCCD.Name = "txtCCCD";
+            this.txtCCCD.Size = new System.Drawing.Size(220, 25);
+            this.txtCCCD.TabIndex = 10;
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSDT.Location = new System.Drawing.Point(547, 51);
+            this.txtSDT.Multiline = true;
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(220, 25);
+            this.txtSDT.TabIndex = 8;
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(146, 266);
+            this.txtDiaChi.Multiline = true;
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(529, 25);
+            this.txtDiaChi.TabIndex = 7;
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label9.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(406, 94);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(119, 20);
+            this.label9.TabIndex = 38;
+            this.label9.Text = "Ngày Đăng Ký";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label8.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(406, 134);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(119, 20);
+            this.label8.TabIndex = 37;
+            this.label8.Text = "CMT / CCCD";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label7.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(406, 51);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 20);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "Số Điện Thoại";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label6.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(7, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 20);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Địa Chỉ";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(332, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 22);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "ĐĂNG KÝ";
+            // 
+            // fResistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(800, 417);
+            this.Controls.Add(this.panelResistration);
+            this.Name = "fResistration";
+            this.Text = "fBillBorrow";
+            this.Load += new System.EventHandler(this.fResistration_Load);
+            this.panelResistration.ResumeLayout(false);
+            this.panelResistration.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelResistration;
+        private System.Windows.Forms.DateTimePicker dateTimePickerNgaySinh;
+        private System.Windows.Forms.DateTimePicker dateTimePickerNgayDangKy;
+        private System.Windows.Forms.TextBox txtTenDocGia;
+        private System.Windows.Forms.TextBox txtMaDocGia;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.TextBox txtTenDangNhap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnCancelBorrow;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.TextBox txtCCCD;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+    }
+}
